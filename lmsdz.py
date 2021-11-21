ceaba74d3dbea1988e4916511ee0dac05792a795
DimaGotsul <94606127+DimaGotsul@users.noreply.github.com>
2021-11-21 13:42:53 +0200

    Add files via upload

---
+++ lmsdz.py
@@ -0,0 +1,23 @@
+#task 1
+
+name = 'Dima'
+day = 'Monday'
+print('Good day',name,'!',day,'is a prefect day to learn some python')
+
+#task 2
+
+surname = 'Hotsul'
+print('Hello',name+'', surname)
+
+#task 3
+
+a = 3
+b = 4
+add = a + b
+sub = a - b
+div = a / b
+mult = a * b
+exp = a ** b
+mod = a % b
+div1 = a // b
+print(add,sub,div,mult,exp,mod,div1)
